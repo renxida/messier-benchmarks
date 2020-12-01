a23e7af5d55bfdc54416c7b8ae736ceed46a48f4
Xida Ren <cedar.ren@gmail.com>
2020-11-30 16:40:43 -0800

    add massif output

---
+++ massif-output/mem1_cp256/run.sh
@@ -0,0 +1,1 @@
+valgrind --tool=massif --time-unit=ms --trace-children=yes --massif-out-file=./massif_%p_output --log-file=./log/massif_%p.log ~/local/sst/sstcore-10.0.0/bin/sst ~/messier-benchmarks/scan_copy.py --model-options="--memory_mb=1 --copy_mb=256"
