ee261e80e281e85ebf9265a2559084e0827061ca
Xida Ren <cedar.ren@gmail.com>
2020-12-08 14:35:45 -0800

    remove confusing file

--- massif-output/run_opal.sh
+++
@@ -1,1 +0,0 @@
-valgrind --tool=massif --time-unit=ms --trace-children=yes --massif-out-file=./massif_%p_output --log-file=./log/massif_%p.log ~/local/sst/sstcore-10.0.0/bin/sst ~/messier-benchmarks/scan_copy.py --model-options="--memory_mb=1 --copy_mb=1024"
